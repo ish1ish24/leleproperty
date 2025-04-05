6f4d40f77e5f65e9b78bc5ad33bfaa0b75a04713
ishan199811 <ishansharma199811@gmail.com>
2025-04-05 17:33:12 +0530

    applicTION

--- LeleProperty/src/main/java/com/leleProperty/LeleProperty/LelePropertyApplication.java
+++ LeleProperty/src/main/java/com/leleProperty/LeleProperty/LelePropertyApplication.java
@@ -3,7 +3,7 @@ package com.leleProperty.LeleProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.leleProperty.LeleProperty")
 public class LelePropertyApplication {
 
 	public static void main(String[] args) {
