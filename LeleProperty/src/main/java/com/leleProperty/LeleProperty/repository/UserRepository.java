176ef70344d01bc95f0c2c649bc58e6f899ba703
ishan199811 <ishansharma199811@gmail.com>
2025-04-05 16:23:58 +0530

    updated pom.xml

--- LeleProperty/src/main/java/com/leleProperty/LeleProperty/repository/UserRepository.java
+++ LeleProperty/src/main/java/com/leleProperty/LeleProperty/repository/UserRepository.java
@@ -1,7 +1,8 @@
 package com.leleProperty.LeleProperty.repository;
 
-import org.springframework.data.jdbc.repository.query.Query;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
